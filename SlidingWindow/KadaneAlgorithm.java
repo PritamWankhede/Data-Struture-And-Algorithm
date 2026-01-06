3b35ceef4ec3ccff9d58dc0ed9b8922ecd7dd511
PritamWankhede <pritamwankhede671@gmail.com>
2026-01-06 22:04:47 +0530

    Added Kadane max subarray solution

---
+++ SlidingWindow/KadaneAlgorithm.java
@@ -0,0 +1,23 @@
+public class KadaneAlgorithm {
+    
+public static int findMaxSumInSubArray(int[] arr){
+     int maxSum = Integer.MIN_VALUE;
+      int currentSum = 0;
+
+      for(int i=0;i<arr.length;i++){
+        currentSum += arr[i];
+        maxSum = Math.max(maxSum, currentSum);
+
+         if(currentSum < 0){
+            currentSum = 0;
+         }
+      }
+     return maxSum;
+}
+
+    public static void main(String[] args){
+     //   int[] arr = {-2,1,-3,4,-1,2,1,-5,4};
+          int[] arr = {1};
+        System.out.print(findMaxSumInSubArray(arr));
+    }
+}
